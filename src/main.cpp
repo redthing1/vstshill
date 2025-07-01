5f77cb4ab3132871a436fa91ffbc2aee1b79c7cf
redthing1 <redthing1@alt.icu>
2025-07-01 16:32:43 -0700

    main: don't error on no args

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,8 @@ redlog::logger log_main = redlog::get_logger("vstshill");
 
 args::Group arguments("arguments");
 args::HelpFlag help_flag(arguments, "help", "help", {'h', "help"});
-args::CounterFlag verbosity_flag(arguments, "verbosity", "verbosity level", {'v'});
+args::CounterFlag verbosity_flag(arguments, "verbosity", "verbosity level",
+                                 {'v'});
 
 void apply_verbosity() {
   int verbosity = args::get(verbosity_flag);
@@ -39,22 +40,28 @@ int main(int argc, char* argv[]) {
   args::Group commands(parser, "commands");
 
   // command parsers
-  args::Command inspect_cmd(commands, "inspect", "inspect and analyze a vst3 plugin", &vstk::cmd_inspect);
-  args::Command gui_cmd(commands, "gui", "open plugin editor gui window", &vstk::cmd_gui);
-  args::Command process_cmd(commands, "process", "process audio files through plugin", &vstk::cmd_process);
-  args::Command scan_cmd(commands, "scan", "scan for vst3 plugins in standard directories", &vstk::cmd_scan);
-  args::Command parameters_cmd(commands, "parameters", "analyze and list plugin parameters", &vstk::cmd_parameters);
+  args::Command inspect_cmd(commands, "inspect",
+                            "inspect and analyze a vst3 plugin",
+                            &vstk::cmd_inspect);
+  args::Command gui_cmd(commands, "gui", "open plugin editor gui window",
+                        &vstk::cmd_gui);
+  args::Command process_cmd(commands, "process",
+                            "process audio files through plugin",
+                            &vstk::cmd_process);
+  args::Command scan_cmd(commands, "scan",
+                         "scan for vst3 plugins in standard directories",
+                         &vstk::cmd_scan);
+  args::Command parameters_cmd(commands, "parameters",
+                               "analyze and list plugin parameters",
+                               &vstk::cmd_parameters);
 
   try {
     parser.ParseCLI(argc, argv);
   } catch (args::Help) {
     std::cout << parser;
-    return 0;
-  } catch (args::ParseError& e) {
-    std::cerr << e.what() << std::endl;
-    std::cerr << parser;
+  } catch (args::Error& e) {
+    std::cerr << e.what() << std::endl << parser;
     return 1;
   }
-
   return 0;
 }
