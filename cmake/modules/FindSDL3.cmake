6c4a32ac60e6cb86c9d240775036906810910768
redthing1 <redthing1@alt.icu>
2025-07-02 22:49:58 -0700

    support optional static sdl3

--- cmake/modules/FindSDL3.cmake
+++ cmake/modules/FindSDL3.cmake
@@ -1,4 +1,12 @@
+# Option to control static linking
+option(VSTSHILL_STATIC_SDL "Link SDL3 statically" OFF)
+
 # find SDL3 with multiple methods for cross-platform compatibility
+if(VSTSHILL_STATIC_SDL)
+    set(SDL3_SHARED_ENABLED_BY_DEFAULT OFF)
+    set(SDL3_STATIC_ENABLED_BY_DEFAULT ON)
+endif()
+
 find_package(SDL3 CONFIG QUIET)
 
 if(NOT SDL3_FOUND)
@@ -6,7 +14,11 @@ if(NOT SDL3_FOUND)
     if(NOT SDL3_FOUND)
         find_package(PkgConfig QUIET)
         if(PkgConfig_FOUND)
-            pkg_check_modules(SDL3 REQUIRED sdl3)
+            if(VSTSHILL_STATIC_SDL)
+                pkg_check_modules(SDL3 REQUIRED sdl3 --static)
+            else()
+                pkg_check_modules(SDL3 REQUIRED sdl3)
+            endif()
         endif()
     endif()
 endif()
@@ -16,7 +28,11 @@ find_package(SDL3_image CONFIG QUIET)
 if(NOT SDL3_image_FOUND)
     find_package(PkgConfig QUIET)
     if(PkgConfig_FOUND)
-        pkg_check_modules(SDL3_IMAGE sdl3-image)
+        if(VSTSHILL_STATIC_SDL)
+            pkg_check_modules(SDL3_IMAGE sdl3-image --static)
+        else()
+            pkg_check_modules(SDL3_IMAGE sdl3-image)
+        endif()
     endif()
 endif()
 
@@ -41,5 +57,12 @@ endif()
 if(HAS_SDL3_IMAGE AND SDL3_image_FOUND AND TARGET SDL3_image::SDL3_image)
     get_target_property(SDL3_IMAGE_INCLUDE_DIRS SDL3_image::SDL3_image INTERFACE_INCLUDE_DIRECTORIES)
     set(SDL3_IMAGE_LIBRARIES SDL3_image::SDL3_image)
+endif()
+
+# Display SDL3 linking status
+if(VSTSHILL_STATIC_SDL)
+    message(STATUS "SDL3 will be linked statically")
+else()
+    message(STATUS "SDL3 will be linked dynamically")
 endif()
 
